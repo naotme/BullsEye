1c4d0e41ee89347427067e89c102bbd4f0886fdc
Toan <naotme@gmail.com>
2019-10-02 23:18:28 -0700

    Initial Commit

---
+++ BullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BullsEye
+//
+//  Created by Toan Nguyen on 10/2/19.
+//  Copyright © 2019 Wildfire Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
