27719b26194d491c68b6c8b3b11c4c3f97bfe16f
Toan <naotme@gmail.com>
2019-10-02 23:24:17 -0700

    Continue: Page 70 (PDF)

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        Text("Welcome to my first app!")
+            .fontWeight(.black)
+            .foregroundColor(.green)
     }
 }
 
