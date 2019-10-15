81e1f49239df10bf971f4f42329f6d6a4910fbf9
naotme <naotme@gmail.com>
2019-10-15 16:42:48 -0700

    Completed: Chapter 8 - The Final App

--- BullsEye/AboutView.swift
+++ BullsEye/AboutView.swift
@@ -16,7 +16,7 @@ struct AboutView: View {
     var body: some View {
         Group {
             VStack {
-                Text("🎯 Bullseye 🎯").modifier(AboutHeadingStyle())
+                Text("🎯 Bullseye 🎯").modifier(AboutHeadingStyle()).navigationBarTitle("About Bullseye")
                 Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.").modifier(AboutBodyStyle()).lineLimit(nil)
                 Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.").modifier(AboutBodyStyle()).lineLimit(nil)
                 Text("Enjoy!").modifier(AboutBodyStyle())
--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -34,7 +34,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Spacer()
+                Spacer().navigationBarTitle("🎯 Bullseye 🎯")
                 
                 // Target row
                 HStack {
@@ -47,7 +47,9 @@ struct ContentView: View {
                 // Slider row
                 HStack {
                     Text("1").modifier(LabelStyle())
-                    Slider(value: $sliderValue, in: 1...100).accentColor(Color.green)
+                    Slider(value: $sliderValue, in: 1...100)
+                        .accentColor(Color.green)
+                        .animation(.easeOut)
                     Text("100").modifier(LabelStyle())
                 }
                 
